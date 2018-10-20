96c4431fb12616fe4ab393525339dd0d8b54deb5
AntoniusKekus <Anton.A.7@gmx.de>
2018-10-20 23:56:18 +0200

    Spiel Erweiterung
    -erster Anfang

--- TicTacToe/TicTacToe/Spiel.cs
+++ TicTacToe/TicTacToe/Spiel.cs
@@ -9,25 +9,60 @@ namespace TicTacToe
 {
     internal class Spiel
     {
+        private int spieler;
+        private string feld;
+        private Aktualisierung aktualisierung;
+        private Eingabe eingabe;
         //das hier ist die Klasse Spiel
         public Spiel()
         {
             //das ist der Konstruktor
-
+            //das Anfangsfeld besteht anfangs nur aus '='
+            feld = "=========";
+            aktualisierung = new Aktualisierung();
+            eingabe = new Eingabe();
         }
 
-        public void Start()
+        private void Start()
         {
-            //zufall wer dran
+            //(x) zufall wer dran
             //neue runde
                 //aktualisiere (spielfeld)
                 //eingabe
                     //abwechseln
                     //überprüfung der eingabe
-                        //ausführung der eingabe(1-9)
+                    //ausführung der eingabe(1-9)
                     //ausgang von eingabe (spiel vorbei?)
-            //
+
+
+
+            Random rdm = new Random();
+            //Zahl zwischen 1-3(3 NICHT eingeschlossen) wird generiert
+            int i = rdm.Next(1,3);
+            if(i == 1)//Wenn die Zahl = 1 ist, ist Spieler 1 dran, sonst Spieler 2
+            {
+                spieler = 1;
+            }//Ende then
+            else
+            {
+                spieler = 2;
+            }//Ende else
+
+            neueRunde();
         }
+
+        private void neueRunde()
+        {
+            //Feld wird aktualisiert
+            aktualisierung.Aktualisiere(feld);
+            //Eingabe
+
+
+        }
+
+        public string gibFeld() => feld;
+
+        public int gibSpieler() => spieler;
 
     }
 }
